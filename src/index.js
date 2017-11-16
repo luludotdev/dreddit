29edc22b995207dd5d4e0b8b96d3782c28b77e0f
Jack Baron <yodude107@gmail.com>
2017-11-16 22:23:17 +0000

    Dupe Checking

--- src/index.js
+++ src/index.js
@@ -16,14 +16,19 @@ const getPost = async () => {
   // Fetch Reddit Posts
   let posts = await fetchPosts(SUBREDDIT)
 
-  let dupeCheck = true
   let dupes = await accessFile()
-  while (dupeCheck) {
+  let IDs = dupes.map(x => x.id)
+  while (true) { // eslint-disable-line
+    if (posts.length) throw new Error('No new posts')
     let id = Math.floor(Math.random() * posts.length)
     let post = posts[id]
-
-    addRow(post.id)
-    return post
+    if (IDs.includes(post.id)) {
+      posts.splice(id, 1)
+      continue
+    } else {
+      addRow(post.id)
+      return post
+    }
   }
 }
 
