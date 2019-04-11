981fbb06397d144e6fad837d9c3623540a6f821e
Jack Baron <jackmbaron@gmail.com>
2019-04-11 06:47:25 +0100

    Rename project

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import signale from 'signale'
 import { config } from './config'
 import PostManager from './manager'
 
+signale.start('Starting dreddit...')
 const managers = config.subreddits.map(post => new PostManager(post))
