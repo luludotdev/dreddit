f5130e8d259dec97c84cf1219a423b2c4d392c88
Jack Baron <jackmbaron@gmail.com>
2020-08-24 09:43:09 +0100

    fix init script

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import 'source-map-support/register'
 
-import { config } from './config'
+import { config } from '~config'
+import { createManager } from '~manager'
+import { mapAsync } from '~utils/arrays'
+import signale from '~utils/signale'
 import { exitHook } from './exitHook'
-import PostManager from './manager'
-import signale from './signale'
-import { mapAsync } from './util'
 
-signale.start('Starting dreddit...')
-const managers = config.subreddits.map(post => new PostManager(post))
+const init = async () => {
+  signale.start('Starting Dreddit...')
+  const managers = await Promise.all(
+    config.subreddits.map(post => createManager(post))
+  )
 
-exitHook(async hook => {
-  await mapAsync(managers, async manager => manager.cleanup())
-  return hook()
-})
+  exitHook(async hook => {
+    await mapAsync(managers, async manager => manager?.cleanup())
+    return hook()
+  })
+}
+
+void init()
