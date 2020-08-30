c96d802350fa520c2ae7c0a2bab2a9ed756a95c6
Jack Baron <jackmbaron@gmail.com>
2020-08-30 21:19:49 +0100

    panic if all subreddits cannot be reached if reddit is down it'll force docker to restart

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register'
 import { config } from '~config'
 import { createManager } from '~manager'
 import { mapAsync } from '~utils/arrays'
-import signale from '~utils/signale'
+import signale, { panic } from '~utils/signale'
 import { exitHook } from './exitHook'
 
 const init = async () => {
@@ -11,6 +11,11 @@ const init = async () => {
   const managers = await Promise.all(
     config.subreddits.map(async post => createManager(post))
   )
+
+  const failures = managers.filter(x => x === undefined)
+  if (failures.length === config.subreddits.length) {
+    return panic('All subreddits could not be reached!')
+  }
 
   exitHook(async hook => {
     await mapAsync(managers, async manager => manager?.cleanup())
