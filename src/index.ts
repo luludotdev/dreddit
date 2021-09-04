3386b5c0df2cb5dbb077f866d84d52e72f96d6e1
Jack Baron <jackmbaron@gmail.com>
2021-09-04 23:36:20 +0100

    update exit hook to flush logs

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import 'source-map-support/register.js'
 
-import { field } from '@lolpants/jogger'
+import { createField, field } from '@lolpants/jogger'
 import process from 'node:process'
 import { config } from '~config/index.js'
-import { ctxField, logger } from '~logger/index.js'
+import { ctxField, errorField, flush, logger } from '~logger/index.js'
 import { createManager } from '~manager/index.js'
 import { mapAsync } from '~utils/arrays.js'
 import { exitHook } from './exitHook.js'
 
-const ctx = ctxField('init')
+const ctx = ctxField('main')
+const action = createField('action')
 
 const init = async () => {
   logger.info(ctx)
@@ -19,12 +20,26 @@ const init = async () => {
 
   const failures = managers.filter((x): x is undefined => x === undefined)
   if (failures.length === config.subreddits.length) {
-    logger.error(ctx, field('message', 'All subreddits could not be reached!'))
+    logger.error(
+      ctx,
+      action('init'),
+      field('message', 'All subreddits could not be reached!')
+    )
+
     process.exit(1)
   }
 
-  exitHook(async exit => {
+  exitHook(async (exit, error) => {
     await mapAsync(managers, async manager => manager?.cleanup())
+
+    const actionField = action('shutdown')
+    if (error) {
+      logger.error(ctx, actionField, errorField(error))
+    } else {
+      logger.info(ctx, actionField)
+    }
+
+    await flush()
     exit()
   })
 }
