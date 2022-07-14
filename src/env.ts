e21a73766443cd1d4bf638e6e955c6bc50a08387
Jack Baron <jackmbaron@gmail.com>
2022-07-14 02:36:58 +0100

    swap out logger

---
+++ src/env.ts
@@ -0,0 +1,20 @@
+import { defineEnvironment, t } from '@lolpants/env'
+
+export const env = defineEnvironment({
+  NODE_ENV: t.string(),
+
+  // #region Application
+  DEBUG_LOGS: t.bool().default(false),
+  IMGUR_CLID: t.string().required(),
+  // #endregion
+
+  // #region Redis
+  REDIS_HOST: t.string(),
+  REDIS_PORT: t.int().default(6379),
+  REDIS_PASS: t.string(),
+  REDIS_DB_OFFSET: t.int().default(0),
+  // #endregion
+})
+
+const IS_PROD = env.NODE_ENV?.toLowerCase() === 'production'
+export const IS_DEV = !IS_PROD
