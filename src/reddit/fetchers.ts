9b4d59fd34fe294df146aea6a20fd6930b05424f
Jack Baron <jackmbaron@gmail.com>
2022-07-24 23:22:44 +0100

    add reddit post logging

--- src/reddit/fetchers.ts
+++ src/reddit/fetchers.ts
@@ -1,5 +1,11 @@
+import { createField, field } from '@lolpants/jogger'
+import { ctxField, logger } from '~/logger.js'
 import { redditAxios as axios } from '~/utils/axios.js'
 import { type PartialPost, type Response, type SortLevel } from './types.js'
+
+const ctx = ctxField('reddit')
+const action = createField('action')
+const status = createField('status')
 
 export const validateSubreddit = async (subreddit: string) => {
   try {
@@ -14,6 +20,14 @@ export const fetchPosts: (
   subreddit: string,
   level?: SortLevel
 ) => Promise<readonly PartialPost[]> = async (subreddit, level = 'hot') => {
+  logger.debug(
+    ctx,
+    action('fetch'),
+    status('preflight'),
+    field('subreddit', `/r/${subreddit}`),
+    field('level', level)
+  )
+
   const resp = await axios.get<Response>(
     `/r/${subreddit}/${level}.json?limit=100`
   )
@@ -27,6 +41,15 @@ export const fetchPosts: (
       title,
       url,
     }))
+
+  logger.debug(
+    ctx,
+    action('fetch'),
+    status('complete'),
+    field('subreddit', `/r/${subreddit}`),
+    field('level', level),
+    field('results', posts.length)
+  )
 
   return posts
 }
