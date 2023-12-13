624d4d69110997b22e0c47b6c058c078f0466442
Jack Baron <jackmbaron@gmail.com>
2023-12-13 21:14:31 +0000

    spoofed ua

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,11 +1,12 @@
 import Axios from 'axios'
-import { readPackageUpSync as readPkg } from 'read-pkg-up'
+// import { readPackageUpSync as readPkg } from 'read-pkg-up'
 import { env } from '~/env.js'
 
-const pkg = readPkg()
-const name = pkg?.packageJson.name ?? 'dreddit'
-const version = pkg?.packageJson.version ?? 'Unknown'
-const userAgent = `${name}/v${version}`
+// const pkg = readPkg()
+// const name = pkg?.packageJson.name ?? 'dreddit'
+// const version = pkg?.packageJson.version ?? 'Unknown'
+// const userAgent = `${name}/v${version}`
+const userAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36`
 
 export const redditAxios = Axios.create({
   baseURL: 'https://www.reddit.com/',
